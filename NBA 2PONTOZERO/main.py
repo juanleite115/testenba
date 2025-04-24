3bcd479c9184e3e43aa51cb05a7c6e725573fb4a
juanleite115 <juan.silva10@fatec.sp.gov.br>
2025-04-24 20:23:53 -0300

    Delete NBA 2PONTOZERO directory

--- NBA 2PONTOZERO/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from scripts.coletar_dados import coletar_dados_time
-from scripts.analisar_dados import gerar_grafico
-
-if __name__ == "__main__":
-    nome_time = "Los Angeles Lakers"
-    coletar_dados_time(nome_time)
-    gerar_grafico(f"data/{nome_time.replace(' ', '_').lower()}_stats.csv")
